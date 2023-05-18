be93998292a37a247cffc0e149bbcf57f62d131e
Kauacnok <kauaprofessionalcontact@gmail.com>
2023-05-18 15:20:01 -0300

    Working with Github authentication on Back-End and Front-End, using cookies, separating Front-End components - NLW Spacetime class 03

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -9,7 +13,12 @@ app.register(cors, {
 	origin: true
 })
 
+app.register(jwt, {
+	secret: 'spacetime_nlw_kauacnok'
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running on http://localhost:3333')
